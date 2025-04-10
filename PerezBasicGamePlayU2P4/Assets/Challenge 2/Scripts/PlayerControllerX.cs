50de0b825076b9ce9da0cdda0a333747877c19a8
ELMilkMan <754989@student.dusd.net>
2025-04-10 11:47:31 -0700

    challenge

---
+++ PerezBasicGamePlayU2P4/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coolDown = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        coolDown = coolDown - Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDown <=0)
+        {
+            coolDown = 1;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
